5937c12d42eb45dbb57644aaad8830a6b0d28ba6
tuxflo <flo.hennig@gmail.com>
2015-03-19 12:32:21 +0100

    update filework function

--- aufg1.c
+++ aufg1.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <stdlib.h>
+
+#define NUM_CHILDS 5
+#define NUM_FILEOPERATIONS 2
 
 int filework(FILE *fp);
 
 int main(int argc, char** argv)
 {
   int pid;
-  FILE *fp;
-  for(int i=0; i<5; i++)
+  int status;
+  int final_number;
+  FILE *fp = NULL;
+  for(int i=0; i<NUM_CHILDS; i++)
   {
     pid = fork();
     if(pid < 0)
@@ -20,18 +26,31 @@ int main(int argc, char** argv)
     else if(pid == 0) // Kind Prozess
     {
       printf("Ich bin das Kind Nummer %i \n", i+1);
-      filework(fp);
-      exit(0);
+      for(int c=0; c<NUM_FILEOPERATIONS; c++)
+      {
+        filework(fp);
+
+        exit(EXIT_SUCCESS);
+      }
     }
     else
     {
-      printf("Ich bin dein Vater!\n");
-      wait();
+      wait(&status);
+      printf("Rückgabewert der Kinder: %d\n", status);
+      fp = fopen("./zahl.dat", "r");
+      if(fp == NULL)
+      {
+        perror("Fehler beim öffnen der Datei");
+        return EXIT_FAILURE;
+      }
+      fscanf(fp, "%d", &final_number);
+      printf("Wert nach Abschluss der Kindprozesse: %d\n", final_number);
     }
   }
   return 0;
 }
 
+//open file, read number, write incremented number, close file
 int filework(FILE *fp)
 {
   int tmp = 0;
@@ -41,10 +60,15 @@ int filework(FILE *fp)
     perror("Fehler beim öffnen der Datei");
     return EXIT_FAILURE;
   }
-  while((tmp = fgetc(fp))!=EOF)
+  fscanf(fp, "%d", &tmp);
+  //printf("eingelesene Zahl: %d\n", tmp);
+  tmp++;
+  rewind(fp);
+  fprintf(fp, "%d", tmp);
+  if(fclose(fp)!=0)
   {
-    printf("%c", tmp);
+    perror("Fehler bei fclose:");
+    exit(EXIT_FAILURE);
   }
-  fclose(fp);
   return 0;
 }
