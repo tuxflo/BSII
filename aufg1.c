7073e5cd8ec2caca2d3eb94334a944c7fa4ef9e8
tuxflo <flo.hennig@gmail.com>
2015-03-19 10:53:03 +0100

    add filework function

--- aufg1.c
+++ aufg1.c
@@ -3,10 +3,12 @@
 #include <sys/types.h>
 #include <stdlib.h>
 
+int filework(FILE *fp);
+
 int main(int argc, char** argv)
 {
   int pid;
-
+  FILE *fp;
   for(int i=0; i<5; i++)
   {
     pid = fork();
@@ -17,12 +19,32 @@ int main(int argc, char** argv)
     }
     else if(pid == 0) // Kind Prozess
     {
-      printf("Ich bin das Kind Nummer %i \n", i+1); exit(0); }
+      printf("Ich bin das Kind Nummer %i \n", i+1);
+      filework(fp);
+      exit(0);
+    }
     else
     {
       printf("Ich bin dein Vater!\n");
       wait();
     }
   }
+  return 0;
+}
+
+int filework(FILE *fp)
+{
+  int tmp = 0;
+  fp = fopen("./zahl.dat", "r+");
+  if(fp == NULL)
+  {
+    perror("Fehler beim öffnen der Datei");
+    return EXIT_FAILURE;
+  }
+  while((tmp = fgetc(fp))!=EOF)
+  {
+    printf("%c", tmp);
+  }
+  fclose(fp);
   return 0;
 }
