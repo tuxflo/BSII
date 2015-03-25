3cd12c5438f5f70284e7767bfb5a73dc3197154d
tuxflo <flo.hennig@gmail.com>
2015-03-25 16:45:06 +0100

    add semaphore initialisation

--- aufg2.c
+++ aufg2.c
@@ -2,71 +2,74 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/sem.h>
 #include <stdlib.h>
 
 #define NUM_CHILDS 5
+#define KEY 123458L
 #define NUM_FILEOPERATIONS 20000
 
-int filework(FILE *fp);
-
-int main(int argc, char** argv)
+void p(int semid)
 {
-  int pid, wpid;
-  int status;
-  int final_number;
-  FILE *fp = NULL;
-  for(int i=0; i<NUM_CHILDS; i++)
-  {
-    pid = fork();
-    if(pid < 0)
-    {
-      perror("Fehler bei fork()");
-      return EXIT_FAILURE;
-    }
-    else if(pid == 0) // Kind Prozess
-    {
-      printf("Ich bin das Kind Nummer %i \n", i+1);
-      for(int c=0; c<NUM_FILEOPERATIONS; c++)
-      {
-        filework(fp);
-      }
-        exit(EXIT_SUCCESS);
-    }
-  }
-  // Vater Prozess
-  waitpid(pid, &status, 0);
-  printf("Rückgabewert der Kinder: %d\n", status);
-
-  semid = semget(KEY, 0, IPC_PRIVATE);
-  if(semid < 0)
-  {
-    perror("Fehler beim Anlegen des Semaphors!");
-    exit(EXIT_FAILURE);
-  }
-  //Semaphore auf 1 setzen (geschlossen)
-  semctl(semid, 0, SETVAL, (int) 1);
-  return 0;
+  printf("P() ausgeführt, Semaphore für kritischen abschnitt gelockt\n");
+  /*semop(semid, 0, 1);*/
 }
 
-//open file, read number, write incremented number, close file
-int filework(FILE *fp)
+void v()
 {
-  int tmp = 0;
-  fp = fopen("./zahl.dat", "r+");
-  if(fp == NULL)
+  printf("V() ausgeführt, Semaphore nach kririschem Abnschitt freigegeben\n");
+}
+int main(int argc, char** argv)
+{
+  int pid, tmp;
+  int status;
+  int semid;
+
+  //einzelnen Sohn erzeugen
+  pid = fork();
+  switch(pid)
   {
-    perror("Fehler beim öffnen der Datei");
-    return EXIT_FAILURE;
+    case -1:
+      perror("Fehler bei fork()");
+      return EXIT_FAILURE;
+
+    case 0: //Sohn Prozess
+      printf("Ich bin der Sohn!\n");
+      v();
+      printf("Semaphore deblockiert\n");
+      exit(EXIT_SUCCESS);
+
+    default: //Vater Prozess
+      semid = semget(KEY, 0, IPC_PRIVATE);
+      if(semid < 0)
+      {
+        //Semaphore existiert noch nicht, anlegen
+        umask(0);
+        semid = semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0700);
+        if(semid < 0)
+        {
+          perror("Fehler beim Anlegen des Semaphors!");
+          exit(EXIT_FAILURE);
+        }
+        printf("Semaphore angelegt! ID:%d \n", semid);
+      }
+      //Semaphore auf 1 setzen (geschlossen)
+      tmp = semctl(semid, 0, SETVAL, (int) 1);
+      if(tmp == -1)
+      {
+        perror("Fehler beim Schliessen des Semaphores");
+        exit(EXIT_FAILURE);
+      }
+      p(semid);
+      waitpid(pid, &status, 0);
+      printf("Rückgabewert des Sohnes: %d\n", status);
+      printf("Semaphore beenden...\n");
+      tmp = semctl(semid, 0, IPC_RMID, 0);
+      if(tmp < 0)
+      {
+        perror("Fehler beim Löschen des Semaphores!\n");
+        exit(EXIT_FAILURE);
+      }
+      return 0;
   }
-  fscanf(fp, "%d", &tmp);
-  //printf("eingelesene Zahl: %d\n", tmp);
-  tmp++;
-  rewind(fp);
-  fprintf(fp, "%d", tmp);
-  if(fclose(fp)!=0)
-  {
-    perror("Fehler bei fclose:");
-    exit(EXIT_FAILURE);
-  }
-  return 0;
 }
