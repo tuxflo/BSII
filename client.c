30af0d787b92b84c9b4d7e3c455d5c6ecf94b951
tuxflo <flo.hennig@gmail.com>
2015-05-28 12:43:52 +0200

    add client.c

--- client.c
+++ client.c
@@ -29,38 +29,5 @@ int main()
     perror("Fehler bei msgget()!\n!");
     exit(EXIT_FAILURE);
   }
-  // read the message from queue
-  while(loop)
-  {
-    rc = msgrcv(msgid, &msg, sizeof(msg.mtext), 0, 0);
-    if (rc < 0)
-    {
-      perror("receive failed!\n");
-      return 1;
-    }
-    else
-    {
-      pid = fork();
-      if(pid < 0)
-      {
-        perror("Fehler bei fork()");
-        return EXIT_FAILURE;
-      }
-      else if(pid == 0) // Kind Prozess
-      {
-        printf("I am a child... \n");
-        // Kind Code
-        printf("received msg: %s\n", msg.mtext);
-        char cp[256];
-        sprintf(cp, "cp %s /tmp", msg.mtext);
-        printf("executing system() with %s\n", cp);
-        exit(EXIT_SUCCESS);
-      }
-      else //Vater Prozess
-      {
-        wait(NULL);
-        return 0;
-      }
-    }
-  }
+  
 }
